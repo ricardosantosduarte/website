bea2796df983038a9f35046c2c1910c9b2b94d17
Ricardo Santos Duarte <ricardosantosduarte1999@gmail.com>
2024-01-18 15:02:35 +0000

    feat: add ga

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,16 +1,30 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 import { getCssText } from '~/stitches.config';
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <Script
+                    src={'/gtm.js'}
+                    id="google-tag-manager"
+                    strategy="afterInteractive"
+                />
                 <style
                     id="stitches"
                     dangerouslySetInnerHTML={{ __html: getCssText() }}
                 />
             </Head>
             <body>
+                <noscript>
+                    <iframe
+                        src={`https://www.googletagmanager.com/ns.html?id=GTM-K9QBLHCL`}
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
                 <Main />
                 <NextScript />
             </body>
