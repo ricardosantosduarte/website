52e5651fd47fab3ed83f8d21151294b45907b70a
Ricardo Santos Duarte <ricardosantosduarte1999@gmail.com>
2024-01-18 15:55:48 +0000

    chore: log

--- src/utils/ga.ts
+++ src/utils/ga.ts
@@ -4,10 +4,14 @@ interface CustomEventProps {
 }
 
 export const event = (event: string, options: Record<string, any>) => {
+    console.log('trying to send');
+
     const customEvent: CustomEventProps = {
         event,
         ...options,
     };
+    console.log(customEvent);
+
     if (window?.dataLayer?.push && customEvent['gtm.uniqueEventId']) {
         window?.dataLayer?.push(customEvent);
     }
